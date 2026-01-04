66a54da805520ffe9f46d2dd49ed12abe8cac1b0
fauzanadity <148694868+fauzanadity@users.noreply.github.com>
2026-01-04 14:05:49 +0700

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -2,5 +2,5 @@
 
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://wwdnqsjcsptucrnlmnpn.supabase.co'
-const supabaseKey = process.env.sb_publishable_M4z-ILfRkeRrqZn_sFabmA_mLdxoY_q
+const supabaseKey = 'sb_publishable_M4z-ILfRkeRrqZn_sFabmA_mLdxoY_q'
 export const supabase = createClient(supabaseUrl, supabaseKey)
