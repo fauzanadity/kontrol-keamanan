e205cc4c4902945a2904d56748a68821b994610c
fauzanadity <148694868+fauzanadity@users.noreply.github.com>
2026-01-04 10:29:45 +0700

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Ambil ini dari Dashboard Supabase > Settings > API
 const supabaseUrl = 'https://wwdnqsjcsptucrnlmnpn.supabase.co'; 
-const supabaseKey = 'sb_secret_zTD5cNbeiuexxPQKSUNi2w_mYrcD2YX';
+const supabaseKey = 'sb_publishable_M4z-ILfRkeRrqZn_sFabmA_mLdxoY_q';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
